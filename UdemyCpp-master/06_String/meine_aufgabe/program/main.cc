aacaeadb260b9e6fbac2f19f5615e4f1d48553a5
joelnyabo <deine-github-email@example.com>
2025-11-23 19:35:13 +0100

    23.11.2025

--- UdemyCpp-master/06_String/meine_aufgabe/program/main.cc
+++ UdemyCpp-master/06_String/meine_aufgabe/program/main.cc
@@ -3,6 +3,8 @@
 #include<fstream>
 #include<filesystem>
 #include<vector>
+#include<string>
+#include<algorithm>
 
 
 struct User {
@@ -11,38 +13,122 @@ struct User {
 };
 
 
+void ausgabe_tab(const std::vector<User> &user)
+{
+    for(auto &it : user)
+    {
+      std::cout<<it.name<<"   "<<it.age<<"\n";
+    }
+
+}
+
+bool max(int a, int b)
+{
+    return a<b ? true:false;
+}
+
 int main()
 {
-    std::vector<User> users(4, User{});
-    std::size_t i = 0;
-   const auto path = std::filesystem::current_path().parent_path() /"data";
-    for(auto it = std::filesystem::directory_iterator(path); it!=std::filesystem::directory_iterator{}; ++it)
+    auto user_tab = std::vector<User>(4, User{});
+    auto my_pfad = std::filesystem::current_path();
+    for(auto it = std::filesystem::directory_iterator(my_pfad); it!=std::filesystem::directory_iterator{}; ++it)
     {
-        std::cout<<*it<<"\n";
-    }
-    auto filename = "user.txt";
+      if(std::filesystem::is_regular_file(*it))
+      {
+        std::cout<<std::filesystem::absolute(*it)<<"  \n->Die Größe = "<<std::filesystem::file_size(*it)<<"\n"<<"Extension = "<<static_cast<std::filesystem::path>(*it).extension()<<"\n";
 
- if(std::filesystem::exists(filename))
- {
-    auto str = std::string{};
-    auto input_file = std::ifstream{};
-    input_file.open(filename, std::ios::in);
-    if(input_file.is_open())
-    {
-        while(getline(std::cin, str))
-        {
-           auto idx = str.find(";");
-           users[i++].name = str.substr(0, idx);
-           users[i++].age =std::stoi(str.substr(idx, str.length()));
-        }
-        std::cout<<users[0].name<<" "<<users[0].age;
+      }
     }
-}
+
+    std::size_t i = 0;
+    auto str = std::string{};
+    auto user = std::string{"users.txt"};
+    auto output_user = std::ifstream{};
+    output_user.open(user, std::ios::in);
+    if(output_user.is_open())
+    {
+        while(getline(output_user, str))
+        {
+            auto idx = str.find(";");
+           user_tab[i].name = str.substr(0, idx);
+           user_tab[i].age = std::stoi(str.substr(idx+1, str.size()));
+           i++;
+        }
+    }
+    output_user.close();
+    ausgabe_tab(user_tab);
+     str = "";
+    //Übung 3.
+
+    auto error = std::string{"error.txt"};
+    auto error_datei = std::ofstream(error);
+
+    auto log = std::string{"log.txt"};
+    auto log_datei = std::ifstream{};
+    log_datei.open(log, std::ios::in);
+    if(log_datei.is_open())
+    {
+        while(getline(log_datei, str))
+        {
+            if(str.find("ERROR") != std::string::npos)
+            {
+                error_datei<<str;
+                std::cout<<str;
+            }
+        }
+    }
     else
     {
-        std::cout<<"Datei konnte icht geöffnet werden!\n";
+        std::cout<<"logdatei kann nicht geöffnet werden!\n";
     }
+    error_datei.close();
+    log_datei.close();
 
+    //aufgabe 4
+    auto number_vector = std::vector<int>{};
+    double summe = 0;
 
+    int at = 0;
+    str = "";
+    auto number = std::string{"numbers.txt"};
+    auto number_datei = std::ifstream{};
+    number_datei.open(number, std::ios::in);
+    if(number_datei.is_open())
+    {
+        while(getline(number_datei, str))
+        {
+          summe += std::stoi(str);
+          std::cout<<str;
+          number_vector.push_back(std::stoi(str));
+          at++;
+        }
+    }
+    else
+    {
+       std::cout<<"\n numbers.txt konnte nicht geöffnet werden \n";
+    }
+    number_datei.close();
+    auto durch = (summe/(static_cast<double>(at)+1));
+   number_datei.close();
+    std::sort(number_vector.begin(), number_vector.end(), max);
+    std::cout<<"\nSumme = "<<summe<<" ,durchschnitt = "<<durch<<" ,Min = "<<number_vector[0]<<" ,Max = "<<number_vector[number_vector.size()-1];
+
+    //Aufgabe 5
+
+    auto backup_path = std::filesystem::current_path();
+    backup_path  /= "backup";
+    if(!std::filesystem::exists(backup_path))
+    {
+        std::filesystem::create_directories(backup_path);
+    }
+    for(auto it = std::filesystem::directory_iterator(std::filesystem::current_path());it!= std::filesystem::directory_iterator{}; ++it)
+    {
+        if(std::filesystem::is_regular_file(*it) && it->path().extension() == ".txt")
+        {
+           backup_path /= static_cast<std::filesystem::path>(*it).filename();
+          std::filesystem::copy_file(static_cast<std::filesystem::path>(*it), backup_path);
+          backup_path= backup_path.parent_path();
+        }
+    }
 
 }
